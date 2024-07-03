ce1b78be1c0860798a20320f51df273da1adc038
“Nicolas <142536049+Nick-Yanase@users.noreply.github.com>
2024-07-03 15:45:12 -0300

    aula 07 typescript

--- 06-nodeJS - Módulo de rede e crypto/datas-horarios.js
+++ 06-nodeJS - Módulo de rede e crypto/datas-horarios.js
@@ -1,4 +1,15 @@
-const { DateTime } = require("luxon");
+const { DateTime, Interval } = require("luxon");
 
 const agora = DateTime.now();
 console.log(agora.month);
+console.log(new Date().toLocaleDateString())//pega a data no padrão do pc
+console.log(new Date().toLocaleDateString("en-US"))
+
+// ---- calcular quantos anos a pessoa tem ----
+
+const dataAniversario = DateTime.fromFormat('18/02/2005', "dd/MM/yyyy")
+console.log(dataAniversario.day)
+console.log(dataAniversario.month)
+
+const idade = Interval.fromDateTimes(dataAniversario, agora).length("year") //pega o intervalo entre duas datas
+console.log(Math.floor(idade)); //mathFloor arredonda para baixo
